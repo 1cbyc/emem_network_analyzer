348d8b69e872bd89733be0455bef9e83bfa48116
1cbyc <isaacnsisong@gmail.com>
2024-09-09 22:33:03 +0100

    building a proper network analyzer i can actually use

--- src/packet_capture.c
+++ src/packet_capture.c
@@ -6,5 +6,7 @@ pcap_t *handle;
 int init_capture() {
     char errbuf[PCAP_ERRBUF_SIZE];
     handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
-    if 
+    if (handle == NULL) {
+        fprintf
+    }
 }
