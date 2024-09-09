6b10a804b084183a485411fe287404c8c7f6e689
1cbyc <isaacnsisong@gmail.com>
2024-09-09 22:38:10 +0100

    building a proper network analyzer i can actually use

--- src/packer_capture.h
+++ src/packer_capture.h
@@ -3,4 +3,6 @@ define PACKET_CAPTURE_H
 
 int init_capture();
 void start_capture();
-void packet_handler(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet)
+void packet_handler(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet);
+
+#endif
