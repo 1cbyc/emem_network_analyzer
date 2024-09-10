ea29647ca0bed1f1e8da71f5ac0dc30908221f39
1cbyc <isaacnsisong@gmail.com>
2024-09-10 16:44:04 +0100

    building a proper network analyzer i can actually use

--- src/packet_capture.h
+++ src/packet_capture.h
@@ -1,6 +1,8 @@
 #ifndef PACKET_CAPTURE_H
 #define PACKET_CAPTURE_H
 
+#include <pcap.h>  // Include pcap.h to define u_char and pcap_pkthdr
+
 int init_capture();
 void start_capture();
 void packet_handler(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet);
