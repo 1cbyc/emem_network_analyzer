ce2ba2ff464f7553b8a6f691d116c519a26a5ee6
1cbyc <isaacnsisong@gmail.com>
2024-09-10 16:49:24 +0100

    building a proper network analyzer i can actually use

--- src/packet_analysis.c
+++ src/packet_analysis.c
@@ -1,6 +1,6 @@
 #include <pcap.h>  // added the pcap.h this time
 #include "packet_analysis.h"
-#include <stdio.h>
+// #include <stdio.h>
 
 void analyze_packet(const u_char *packet) {
     // will implement the packet analysis logic here shortly
